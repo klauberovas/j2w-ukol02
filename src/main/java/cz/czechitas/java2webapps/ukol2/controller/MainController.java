3fd8406195dbb74887e4b05bfa2fa51c4df6fd08
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2025-10-01 11:35:39 +0200

    add alt text to images (ftlh)

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -40,7 +41,23 @@ public class MainController {
             int index = randomGenerator.nextInt(quotes.size());
 
             modelAndView.addObject("quote", quotes.get(index));
-            modelAndView.addObject("image", index + 1);
+
+            Map<Integer, String> imageAlt = Map.of(
+                    1, "Laptop on the desk",
+                    2, "Laptop with green screen",
+                    3, "Robot C-3PO from Star Wars",
+                    4, "Pumpkin market",
+                    5, "Autumn forest",
+                    6, "Beach by the sea",
+                    7, "A cup of tea",
+                    8, "Toadstools in the forest"
+            );
+
+            int imageNumber = index + 1;
+            String altText = imageAlt.get(imageNumber);
+
+            modelAndView.addObject("image", imageNumber);
+            modelAndView.addObject("altText", altText);
 
     } catch (IOException e) {
             modelAndView.addObject("quote", "Citát se nepodařilo najít");
